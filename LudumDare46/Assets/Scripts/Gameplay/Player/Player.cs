76c4b402ef2466e755182af25fcaabe1f4b4aae4
frankvega2010 <franco_vega@msn.com>
2020-04-18 15:41:37 -0300

    Added initial player movement & WIP Shields

---
+++ LudumDare46/Assets/Scripts/Gameplay/Player/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public LayerMask Mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, 999, Mask))
+        {
+            Vector3 lookPos = hit.point - transform.position;
+            lookPos.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = rotation;
+        }
+    }
+
+    private void Move()
+    {
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ LudumDare46/Assets/Scripts/Gameplay/Player/Shield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    //public float speed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookPos = target.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = rotation;
+    }
+}
