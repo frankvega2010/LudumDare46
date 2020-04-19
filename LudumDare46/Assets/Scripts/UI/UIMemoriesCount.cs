2dacff5f344f091a8a68e8cfbaa7323925bc2a13
frankvega2010 <franco_vega@msn.com>
2020-04-19 15:09:45 -0300

    Improved UI

---
+++ LudumDare46/Assets/Scripts/UI/UIMemoriesCount.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMemoriesCount : MonoBehaviour
+{
+    public Text memoriesText;
+    public CollectibleManager collectiblesManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CollectibleManager.OnCollectibleGrabbed += UpdateText;
+        //UpdateText(true);
+    }
+
+    /*// Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    public void UpdateText(bool isPlayerAlive)
+    {
+        //Debug.Log((collectiblesManager.collectiblesLeft - collectiblesManager.collectiblesAmount));
+
+        memoriesText.text = (collectiblesManager.collectiblesLeft - collectiblesManager.collectiblesAmount) * -1 + 
+            "/" + collectiblesManager.collectiblesAmount + " Memories";
+    }
+
+    private void OnDestroy()
+    {
+        CollectibleManager.OnCollectibleGrabbed -= UpdateText;
+    }
+}
