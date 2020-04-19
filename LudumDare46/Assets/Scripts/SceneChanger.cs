8eb3a2bedae92c84de430dd126a36255d99693cb
frankvega2010 <franco_vega@msn.com>
2020-04-19 05:21:10 -0300

    Added Menu navigation.

---
+++ LudumDare46/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneToChange);
+    }
+
+    public void ChangeScene(string newScene)
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
---
+++ LudumDare46/Assets/Scripts/Utils/GoToLink.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToLink : MonoBehaviour
+{
+    public string link;
+
+    public void GoToThisLink()
+    {
+        Application.OpenURL(link);
+    }
+
+    public void GoToThisLink(string newLink)
+    {
+        Application.OpenURL(newLink);
+    }
+}
